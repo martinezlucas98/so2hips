151a16a3897540b9af47188bae64f9ce211f3e43
Lucas Martinez <lucasoctaviom@gmail.com>
2020-06-07 23:23:30 -0400

    Agrega prototipos de varias de las funciones necesarias

--- so2hips.py
+++ so2hips.py
@@ -1,10 +1,13 @@
 import os
 import subprocess
+import psutil
+
 
 def is_program(name):
-        """Ve si el archivo `name` existe"""
+        #Ve si el archivo `name` existe
         from shutil import which
         print (which(name)) #is not None
+        print ('\n')
 
 #print(is_program("wget"))
 
@@ -12,13 +15,91 @@ def check_files_is_program(list):
         #nothing yet
         print('')
 
-def check_mailq(command):
-        p = subprocess.Popen("mailq", stdout=subprocess.PIPE, shell=True)
-        (output, err) = p.communicate()
-        print(output)
+def check_mailq(MAX_Q_COUNT):
+	p = subprocess.Popen("mailq", stdout=subprocess.PIPE, shell=True)
+	(output, err) = p.communicate()
+	print(output.decode("utf-8"))
+	#print('\n')
+	mail_list = output.decode("utf-8").splitlines()
+	if len(mail_list) > MAX_Q_COUNT:
+		print("Do something\n")
+
+
+def check_promisc_devs(P_DIR ):
+	p_off = subprocess.Popen("cat "+P_DIR+" | grep \"left promisc\"", stdout=subprocess.PIPE, shell=True)
+	(output_off, err) = p_off.communicate()
+	data_off = output_off.decode("utf-8")
+	list_off = data_off.splitlines()
+	l_off = len(list_off)
+	
+	p_on = subprocess.Popen("cat "+P_DIR+" | grep \"entered promisc\"", stdout=subprocess.PIPE, shell=True)
+	(output_on, err) = p_on.communicate()
+	data_on = output_on.decode("utf-8")
+	list_on = data_on.splitlines()
+	l_on = len(list_on)
+	
+	if l_off != l_on:
+		#print("Smth is in promisc mode\n")
+		compare = []
+		devices_on = []
+		for line in list_off:
+			compare.append(line.split()[-4])
+		for line in list_on:
+			compare.append(line.split()[-4])
+		
+		dict_counter = {i:compare.count(i) for i in compare}
+		for device in dict_counter:
+			if dict_counter[device]%2 != 0:
+				devices_on.append(device)
+		for device in devices_on:
+			print(''+device+' :: Promiscuous mode ON\n')
+
+def check_promisc_apps(P_APP_LIST):
+	p = subprocess.Popen("ps axo pid,command | grep -E '"+P_APP_LIST+"' | grep -v '"+P_APP_LIST+"'", stdout=subprocess.PIPE, shell=True)
+	(output, err) = p.communicate()
+	print(output.decode("utf-8"))
+	print('\n')	
+
+def check_promisc(P_DIR, P_APP_LIST):
+	check_promisc_devs(P_DIR)
+	check_promisc_apps(P_APP_LIST)
+	
+	
+def process_usage(PROCESS_USAGE_LIMITS):
+	process_list = list()
+	for proc in psutil.process_iter():
+		process_dict = proc.as_dict(attrs=['pid', 'name', 'cpu_percent', 'memory_percent', 'create_time'])
+		process_list.append(process_dict)
+		
+	for proc in process_list:
+		print(proc)
+	print('\n')
+	
+	
+def check_ip_connected():
+	IPs_connected = subprocess.Popen("netstat -tu 2>/dev/null", stdout=subprocess.PIPE, shell=True)
+			
+	
+def main():
+	MAX_Q_COUNT = 1000
+	P_DIR = '/var/log/messages'
+	P_APP_LIST = 'wireshark|tcpdump'
+	PROCESS_USAGE_LIMITS = '' #name cpu memory time
+	
+	if os.path.isdir(P_DIR) is not True:
+		P_DIR = '/var/log/syslog'
+	#print(''+P_DIR+'\n')
+	#ciclo main
+	check_mailq(MAX_Q_COUNT)
+	#is_program("wget")
+	#is_program("asdfgh")
+	check_promisc(P_DIR, P_APP_LIST)
+	process_usage()
+	
+	return(0)
+        
+        
 
 if __name__=='__main__':
-        #ciclo main
-        check_mailq("mailq")
-        is_program("wget")
-        is_program("asdfgh")
+        main()
+        
